82e961a6f34d18516f29c1e7a6e6eb73d8f7a7ca
Elisei Bobocea <elisey93@icloud.com>
2022-12-26 19:47:34 +0000

    Initial Commit

---
+++ Blackjack/BlackjackApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlackjackApp.swift
+//  Blackjack
+//
+//  Created by Elisei Bobocea on 26/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct BlackjackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
